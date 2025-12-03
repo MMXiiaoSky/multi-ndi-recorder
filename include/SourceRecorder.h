a9ec00f961d3faf071a022d81a2d0f9f8c4eb853
XiiaoSky <106652310+MMXiiaoSky@users.noreply.github.com>
2025-12-03 08:34:05 +0800

    Stabilize frame pacing when NDI timestamps misbehave

--- include/SourceRecorder.h
+++ include/SourceRecorder.h
@@ -56,7 +56,13 @@ private:
     QAtomicInteger<bool> m_paused;
     QAtomicInteger<bool> m_recordingStarted;
     qint64 m_videoPts = 0;
+    qint64 m_lastWriterPts = -1;
     qint64 m_firstTimestamp = NDIlib_recv_timestamp_undefined;
+    qint64 m_prevTimestamp = NDIlib_recv_timestamp_undefined;
+    qint64 m_timestampScale = 10000000; // ticks per second (10ns default)
+    qint64 m_expectedFrameTicks10ns = 0;
+    qint64 m_expectedPtsStep = 1;
+    bool m_timestampOutlierLogged = false;
     QImage m_preview;
     QString m_status;
     QElapsedTimer m_timer;
